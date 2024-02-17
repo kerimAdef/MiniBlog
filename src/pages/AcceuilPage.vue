15b52e1a124e6e31d7ea065ac695031ab6dd0374
mouhamed sarr <148370657+fuelz99@users.noreply.github.com>
2024-02-17 18:14:30 +0100

    sarr

---
+++ src/pages/AcceuilPage.vue
@@ -0,0 +1,66 @@
+<template>
+    <q-page>
+      <q-list bordered separator>
+        <q-item
+          v-for="post in posts"
+          :key="post.id"
+          clickable
+          @click="openPost(post)"
+        >
+          <q-item-section avatar>
+            <q-avatar>
+              <img :src="post.authorAvatarUrl" alt="Avatar">
+            </q-avatar>
+          </q-item-section>
+  
+          <q-item-section>
+            <q-item-label>
+              {{ post.title }}
+            </q-item-label>
+            <q-item-label caption>
+              {{ post.author }} - {{ post.date }}
+            </q-item-label>
+            <q-item-label lines="2">
+              {{ post.content }}
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-page>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        posts: [
+          {
+            id: 1,
+            title: 'Titre de l\'article 1',
+            author: 'Auteur 1',
+            authorAvatarUrl: 'url_de_l_avatar_1',
+            date: '17/02/2024',
+            content: 'Contenu de l\'article 1'
+          },
+          {
+            id: 2,
+            title: 'Titre de l\'article 2',
+            author: 'Auteur 2',
+            authorAvatarUrl: 'url_de_l_avatar_2',
+            date: '17/02/2024',
+            content: 'Contenu de l\'article 2'
+          },
+          // Ajoutez d'autres articles ici...
+        ]
+      }
+    },
+    methods: {
+      openPost(post) {
+        // Méthode pour ouvrir un article individuel
+        console.log('Ouvrir l\'article:', post.title);
+        // Vous pouvez implémenter ici la logique pour ouvrir un article complet
+      }
+    }
+  }
+  </script>
+  
---
+++ src/pages/MessengerPage.vue
@@ -0,0 +1,19 @@
+<!--
+Forked from:
+https://quasar.dev/vue-components/chat#example--name
+-->
+<div id="q-app" style="min-height: 100vh;">
+    <div class="q-pa-md row justify-center">
+        <div style="width: 100%; max-width: 400px">
+          <q-chat-message
+            name="me"
+            :text="['hey, how are you?']"
+            sent
+          ></q-chat-message>
+          <q-chat-message
+            name="Jane"
+            :text="['doing fine, how r you?']"
+          ></q-chat-message>
+        </div>
+      </div>
+    </div>
